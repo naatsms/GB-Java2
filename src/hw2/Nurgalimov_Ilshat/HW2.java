6aa4203255f842e9957b04171e68a33d87dd8f26
NURGALIMOV <il.nur86@mail.ru>
2017-10-29 20:02:12 +0300

    Добавил проверку длины массива

--- src/hw2/Nurgalimov_Ilshat/HW2.java
+++ src/hw2/Nurgalimov_Ilshat/HW2.java
@@ -30,8 +30,8 @@ public class HW2 {
         String[][] array = new String[4][4];
         //Заполняем массив array
         int a = 0;
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
+        for(int i = 0; i < 4; i++) {
+            for(int j = 0; j < 4; j++) {
                 array[i][j] = Integer.toString(a);
                 a++;
             }
@@ -40,7 +40,7 @@ public class HW2 {
         //Заполняем массив textArray из файла
         try {
             int b;
-            FileReader file = new FileReader("C://JAVA/GB/Java2/2/HW2/src/text.txt");
+            FileReader file = new FileReader("text.txt");
             char text = ' ';
             for(int i = 0; i < 4; i++){
                 for(int j = 0; j < 4; j++){
@@ -69,8 +69,7 @@ public class HW2 {
             } catch(ArithmeticException ex) {
                 ex.printStackTrace();
             }
-
-
+            System.out.println("End of the program!");
         }
     }
 
@@ -78,16 +77,18 @@ public class HW2 {
         int sum = 0;
         int indexArray1 = 0;
         int indexArray2 = 0;
-        if(array.length != 4 || array[0].length != 4) throw new ArrayIndexOutOfBoundsException();
+        if(array.length != 4 || array[0].length != 4 || array[1].length != 4 || array[2].length != 4 || array[3].length != 4) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
         try {
-            for(int i = 0; i < 4; i++){
-                for(int j = 0; j < 4; j++){
+            for(int i = 0; i < array.length; i++) {
+                for(int j = 0; j < array[0].length; j++){
                     indexArray1 = i;
                     indexArray2 = j;
                     sum += parseInt(array[i][j]);
                 }
             }
-        } catch (NumberFormatException exception){
+        } catch (NumberFormatException exception) {
             throw new ArithmeticException("In the cell is not a number, a cell index [" + indexArray1 + "] ["
                     + indexArray2 + "]");
         }
