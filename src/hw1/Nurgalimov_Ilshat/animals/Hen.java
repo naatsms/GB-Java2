6a72c209604095fac85226121726a54f2cca157e
NURGALIMOV <il.nur86@mail.ru>
2017-10-30 13:53:55 +0300

    В классах добавлен дополнительный конструктор

--- src/hw1/Nurgalimov_Ilshat/animals/Hen.java
+++ src/hw1/Nurgalimov_Ilshat/animals/Hen.java
@@ -6,12 +6,19 @@ package animals;
  * @version (a version number or a date)
  */
 public class Hen extends Animal implements Jumpable {
+    private final int RUN_LIMIT = 100;
+    private final float JUMP_LIMIT = 10f;
     private float jump_limit;
 
     public Hen(String name) {
         this.name = name;
-        this.run_limit = 100;
-        jump_limit = 10f;
+        this.run_limit = RUN_LIMIT;
+        jump_limit = JUMP_LIMIT;
+    }
+    public Hen(String name, int run_limit, float jump_limit) {
+        this.name = name;
+        this.run_limit = run_limit;
+        this.jump_limit = jump_limit;
     }
 
     @Override
