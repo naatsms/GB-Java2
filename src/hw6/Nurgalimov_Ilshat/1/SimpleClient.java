a6346785c686dee3178225538eb595a9c396c718
NURGALIMOV <il.nur86@mail.ru>
2017-11-12 23:07:51 +0300

    GB JAVA 2 Урок 6 первый пункт ДЗ: Модифицировать код SimpleClient так, чтобы клиент мог читать сообщения от сервера. Для реализации добавил BufferedReader reader.

---
+++ src/hw6/Nurgalimov_Ilshat/1/SimpleClient.java
@@ -0,0 +1,56 @@
+/**
+ * Java. Level 2. Lesson 6. My homework.
+ * Simple chat client
+ *
+ * @author Sergey Iryupin
+ * @modified Ilshat Nurgalimov
+ * @version dated 12.11.2017
+ * @task 6
+ * @mark
+ */
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+class SimpleClient {
+
+    final String SERVER_ADDR = "127.0.0.1"; // or "localhost"
+    final int SERVER_PORT = 2048;
+    final String CLIENT_PROMPT = "$ ";
+    final String CONNECT_TO_SERVER = "Connection to server established.";
+    final String CONNECT_CLOSED = "Connection closed.";
+    final String EXIT_COMMAND = "exit"; // command for exit
+
+    Socket socket;
+    Scanner scanner;
+    PrintWriter writer;
+    BufferedReader reader;
+    String message;
+
+    public static void main(String[] args) {
+        new SimpleClient();
+    }
+
+    SimpleClient() {
+        scanner = new Scanner(System.in); // for keyboard input
+        try {
+            socket = new Socket(SERVER_ADDR, SERVER_PORT);
+            writer = new PrintWriter(socket.getOutputStream());
+            System.out.println(CONNECT_TO_SERVER);
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            do {
+                System.out.print(CLIENT_PROMPT);
+                message = scanner.nextLine();
+                writer.println(message);
+                writer.flush();
+                System.out.println(reader.readLine());
+            } while (!message.equals(EXIT_COMMAND));
+            writer.close();
+            socket.close();
+            reader.close();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        System.out.println(CONNECT_CLOSED);
+    }
+}
